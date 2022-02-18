2998c6f6d438494d00a419e9b5972ac655728a65
Jatin Nagar <itsjatinnagar@gmail.com>
2022-02-18 12:32:44 +0530

    code formatting

--- app.js
+++ app.js
@@ -1,49 +1,32 @@
 // DOM Elements
 const resultDisplay = document.getElementById("result"),
-    passRange = document.getElementById("slider"),
-    passLen = document.getElementById("length"),
-    lowerInput = document.getElementById("lower"),
-    upperInput = document.getElementById("upper"),
-    numberInput = document.getElementById("number"),
-    symbolInput = document.getElementById("symbol"),
-    generatePassBtn = document.getElementById("generate");
-
-// Slider Styling
-const slider = () => {
-    passLen.innerText = passRange.value;
-    let gradPerc = (100 * (passRange.value - 8)) / (38 - 8);
-    passRange.style.background = `linear-gradient(to right, hsl(237, 53%, 55%) ${gradPerc}%, hsla(0, 0%, 100%, 0.314) ${gradPerc}%)`;
-};
+    includeLower = document.getElementById("lower").checked,
+    includeUpper = document.getElementById("upper").checked,
+    includeNumber = document.getElementById("number").checked,
+    includeSymbol = document.getElementById("symbol").checked;
 
 // Array from Range
 const arrayFromRange = (lower, upper) => {
     let list = [];
     for (let i = lower; i <= upper; i++) {
-        list.push(i);
+        list.push(String.fromCharCode(i));
     }
     return list;
 };
 
-// Get Randoms
-const lowerAlphaCodes = arrayFromRange(97, 122);
-
-const upperAlphaCodes = arrayFromRange(65, 90);
-
-const numberCodes = arrayFromRange(48, 57);
-
-const symbolCodes = arrayFromRange(33, 47)
+const lowerAlphabets = arrayFromRange(97, 122);
+const upperAlphabets = arrayFromRange(65, 90);
+const numbers = arrayFromRange(48, 57);
+const symbols = arrayFromRange(33, 47)
     .concat(arrayFromRange(58, 64))
     .concat(arrayFromRange(91, 96))
     .concat(arrayFromRange(123, 126));
 
 // Generate Password
 const generatePassword = () => {
-    let hasLower = lowerInput.checked,
-        hasUpper = upperInput.checked,
-        hasNumber = numberInput.checked,
-        hasSymbol = symbolInput.checked,
-        totalCount = hasLower + hasUpper + hasNumber + hasSymbol,
-        includeCodes = [],
+    let totalCount =
+            includeLower + includeUpper + includeNumber + includeSymbol,
+        characterSet = [],
         index = passRange.value,
         password = "";
 
@@ -51,47 +34,24 @@ const generatePassword = () => {
         resultDisplay.value = "Minimum 1 setting required";
         return;
     } else {
-        if (hasLower) {
-            includeCodes = includeCodes.concat(lowerAlphaCodes);
+        if (includeLower) {
+            characterSet = characterSet.concat(lowerAlphabets);
         }
-        if (hasUpper) {
-            includeCodes = includeCodes.concat(upperAlphaCodes);
+        if (includeUpper) {
+            characterSet = characterSet.concat(upperAlphabets);
         }
-        if (hasNumber) {
-            includeCodes = includeCodes.concat(numberCodes);
+        if (includeNumber) {
+            characterSet = characterSet.concat(numbers);
         }
-        if (hasSymbol) {
-            includeCodes = includeCodes.concat(symbolCodes);
+        if (includeSymbol) {
+            characterSet = characterSet.concat(symbols);
         }
     }
 
     for (let i = 0; i < index; i++) {
-        let randInt = Math.floor(Math.random() * includeCodes.length);
-        password += String.fromCharCode(includeCodes[randInt]);
+        let randInt = Math.floor(Math.random() * characterSet.length);
+        password += characterSet[randInt];
     }
 
     resultDisplay.value = password;
 };
-
-// DOM Events
-window.addEventListener("load", () => {
-    slider();
-    generatePassword();
-});
-passRange.addEventListener("input", slider);
-generatePassBtn.addEventListener("click", () => {
-    generatePassword();
-});
-
-// Copy To Clipboard
-const copyBtn = document.getElementById("copy-btn");
-copyBtn.addEventListener("click", () => {
-    copyBtn.classList.remove("far");
-    copyBtn.classList.add("fas");
-    setTimeout(() => {
-        copyBtn.classList.remove("fas");
-        copyBtn.classList.add("far");
-    }, 2000);
-
-    navigator.clipboard.writeText(resultDisplay.value);
-});
---
+++ script.js
@@ -0,0 +1,35 @@
+const passRange = document.getElementById("slider"),
+    passLen = document.getElementById("length"),
+    generatePassBtn = document.getElementById("generate"),
+    copyBtn = document.getElementById("copy-btn");
+
+// Slider Styling
+const slider = () => {
+    passLen.innerText = passRange.value;
+    const gradPerc = (100 * (passRange.value - 8)) / (38 - 8);
+    passRange.style.background = `linear-gradient(to right, hsl(237, 53%, 55%) ${gradPerc}%, hsla(0, 0%, 100%, 0.314) ${gradPerc}%)`;
+};
+
+// DOM Events
+window.addEventListener("load", () => {
+    slider();
+    generatePassword();
+});
+
+// Password Length Slider
+passRange.addEventListener("input", slider);
+
+// Generate Password Button
+generatePassBtn.addEventListener("click", generatePassword);
+
+// Copy To Clipboard
+copyBtn.addEventListener("click", () => {
+    copyBtn.classList.remove("far");
+    copyBtn.classList.add("fas");
+    setTimeout(() => {
+        copyBtn.classList.remove("fas");
+        copyBtn.classList.add("far");
+    }, 2000);
+
+    navigator.clipboard.writeText(resultDisplay.value);
+});
