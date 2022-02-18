fe4a37db36fac4b4f3c88a8ad8af53b0328a4128
Jatin Nagar <itsjatinnagar@gmail.com>
2022-02-18 18:56:32 +0530

    version 3

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ const resultDisplay = document.getElementById("result"),
     includeNumber = document.getElementById("number").checked,
     includeSymbol = document.getElementById("symbol").checked;
 
+// Shuffle Array Elements
+const shuffle = (list) => {
+    let shuffledPassword = [];
+    let length = list.length;
+    while (length > 0) {
+        let randInt = Math.floor(Math.random() * length);
+        shuffledPassword.push(list[randInt]);
+        length--;
+    }
+    return shuffledPassword;
+};
+
 // Array from Range
 const arrayFromRange = (lower, upper) => {
     let list = [];
@@ -28,7 +40,7 @@ const generatePassword = () => {
             includeLower + includeUpper + includeNumber + includeSymbol,
         characterSet = [],
         index = passRange.value,
-        password = "";
+        password = [];
 
     if (totalCount === 0) {
         resultDisplay.value = "Minimum 1 setting required";
@@ -36,22 +48,39 @@ const generatePassword = () => {
     } else {
         if (includeLower) {
             characterSet = characterSet.concat(lowerAlphabets);
+            // Ensure one lower alphabet is included
+            let randInt = Math.floor(Math.random() * lowerAlphabets.length);
+            password.push(lowerAlphabets[randInt]);
+            index--;
         }
         if (includeUpper) {
             characterSet = characterSet.concat(upperAlphabets);
+            // Ensure one upper alphabet is included
+            let randInt = Math.floor(Math.random() * upperAlphabets.length);
+            password.push(upperAlphabets[randInt]);
+            index--;
         }
         if (includeNumber) {
             characterSet = characterSet.concat(numbers);
+            // Ensure one number is included
+            let randInt = Math.floor(Math.random() * numbers.length);
+            password.push(numbers[randInt]);
+            index--;
         }
         if (includeSymbol) {
             characterSet = characterSet.concat(symbols);
+            // Ensure one symbol is included
+            let randInt = Math.floor(Math.random() * symbols.length);
+            password.push(symbols[randInt]);
+            index--;
         }
     }
 
     for (let i = 0; i < index; i++) {
+        characterSet = shuffle(characterSet);
         let randInt = Math.floor(Math.random() * characterSet.length);
-        password += characterSet[randInt];
+        password.push(characterSet[randInt]);
     }
 
-    resultDisplay.value = password;
+    resultDisplay.value = password.join("");
 };
